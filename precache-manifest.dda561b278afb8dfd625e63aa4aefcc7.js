b3aef84e2d6015ede2b517b300f06ec5c3b4ec40
Slava <ogurezslava@gmail.com>
2019-01-08 01:44:28 +0300

    Updates

---
+++ precache-manifest.dda561b278afb8dfd625e63aa4aefcc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aee4ecd23dff1136f65",
+    "url": "/currency-table/static/js/runtime~main.2aee4ecd.js"
+  },
+  {
+    "revision": "f1b1a2e853f99a376ca5",
+    "url": "/currency-table/static/js/main.f1b1a2e8.chunk.js"
+  },
+  {
+    "revision": "66b4d8a120133cc267cc",
+    "url": "/currency-table/static/js/1.66b4d8a1.chunk.js"
+  },
+  {
+    "revision": "f1b1a2e853f99a376ca5",
+    "url": "/currency-table/static/css/main.364acdcf.chunk.css"
+  },
+  {
+    "revision": "d5f08e73743b0834900e750d97b03ce4",
+    "url": "/currency-table/index.html"
+  }
+];
